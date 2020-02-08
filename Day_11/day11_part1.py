4cad052cb86acae187864d6c7f699313b526f5ae
scott-p-lane <scott.patrick.lane@gmail.com>
2020-02-08 14:29:54 -0500

    Trying to fix the last commit. Doesn't look like I can push from Eclipse when I have MFA. I'll just commit locally and use GitHub client to push later.

--- Day_11/day11_part1.py
+++ Day_11/day11_part1.py
@@ -4,24 +4,24 @@ Created on Feb 3, 2020
 @author: slane
 '''
 from aoc_helpers.IntCode import IntCode
-from aoc_helpers.GridMap import GridMap
+from aoc_helpers.GridCoordinates import GridCoordinates
+from aoc_helpers.GridCoordinates import GridOrientation
+from aoc_helpers.GridCoordinates import TurnDirection
 
 #define some handy constants to use in the program
 black = 0
 white = 1
-left = 0
-right = 1
-direction = 'u'
+direction = GridOrientation.up
 
 program = IntCode("input_part1.txt")
-grid = GridMap(10000,10000,black)
+grid = GridCoordinates()
 
 
 paint_count = 0
-turn_count = 0
+overlap_count = 0
 while program.executeUntil('03'):
     #Pass in color of the current cell.
-    program.opInputs.append(int(grid.getCellValue()))
+    program.opInputs.append(grid.getCoordinateValue()[-1])
     
     #Get the color to paint the cell.
     program.executeUntil('04')
@@ -31,13 +31,19 @@ while program.executeUntil('03'):
     #Get the direction to advance the point to
     program.executeUntil('04')
     program.executeOperation()
-    if program.opOutput == left:
-        grid.changeOrientation("l")
+    if program.opOutput == TurnDirection.left.value:
+        grid.changeOrientation(TurnDirection.left)
     else:
-        grid.changeOrientation("r")
+        grid.changeOrientation(TurnDirection.right)
         
     #Paint and advance 1
-    grid.setCurrCellTo(tileColor)
-    grid.advanceToNextCell()
+    paint_count += 1
+    if grid.processedCoordinate()[-1]:
+        print("Already processed coordinate: ",grid.processedCoordinate())
+        overlap_count += 1
+    grid.setCoordinateValue(tileColor)
+    grid.advance(1)
+    
 print("DONE!! ")
-print("Painted squares: ", paint_count, " Turns: ", turn_count)
+print("Painted squares: ", paint_count, " Overlap: ", overlap_count)
+print("Tiles painted at least once: ",str(paint_count - overlap_count))
